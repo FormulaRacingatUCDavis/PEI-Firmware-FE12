4bcd566ce88e1352fc75ea67635f271671f44173
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2025-02-13 17:39:43 -0800

    Fixed out-of-bound access in BMS firmware
    Decrementing for loop was using unsigned int for the counter. Since
    end condition is being less than 0, but counter was unsigned, counter
    would overflow and not satisfy end condition, leading to out-of-bound
    accesses.

--- PEI_Firmware_FE12_Rev1/Core/Src/ADBMS6830.c
+++ PEI_Firmware_FE12_Rev1/Core/Src/ADBMS6830.c
@@ -160,7 +160,7 @@ static void single_spi_write(SPI_HandleTypeDef* const hspi_ptr, // Pointer to th
 		uint8_t idx = 4;
 
 		// Data written to last IC in the chain first and first IC in the chain last (see bus protocols in datasheet)
-		for (uint8_t ic = N_OF_ADBMS - 1; ic >= 0; ic--) {
+		for (int8_t ic = N_OF_ADBMS - 1; ic >= 0; ic--) {
 			uint16_t data_pec;
 
 			for (uint8_t i = 0; i < 6; i++) {
@@ -220,7 +220,7 @@ static void dual_spi_write(SPI_HandleTypeDef* const hspi_ptr, // Pointer to the 
 		uint8_t idx = 4;
 
 		// Data packaging in the forward direction
-		for (uint8_t ic = comm_bk_id; ic >= 0; ic--) {
+		for (int8_t ic = comm_bk_id; ic >= 0; ic--) {
 			for (uint8_t i = 0; i < 6; i++) {
 				tx_data_fwd[idx] = data[ic][i];
 				idx++;
